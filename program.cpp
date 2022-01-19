33bf2d369a3ee9c3fed417f91a061c049c453c54
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-01-19 20:54:53 +0500

    Added Ahsan as admin

--- program.cpp
+++ program.cpp
@@ -3,5 +3,9 @@ using namespace std;
 
 int main(){
     cout << "Welcome Sufiyaan";
+<<<<<<< HEAD
+=======
+    cout << "Welcome Ahsan";
+>>>>>>> parent of ccf213a (added Qasim as admin)
     return 0;
 }
