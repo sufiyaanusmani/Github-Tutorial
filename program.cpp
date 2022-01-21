d76ee5013ef89424bf2e3b4c371aa3940eab4885
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2022-01-21 21:10:58 +0500

    Fixed bugs in customer portal

--- program.cpp
+++ program.cpp
@@ -5,7 +5,7 @@ int main(){
     cout << "Welcome Sufiyaan";
     cout << "Welcome Ahsan";
 
-    cout << "Customer Portal Improved Version";
-
+    cout << "Customer Portal Improved Version 2.0";
+    
     return 0;
 }
